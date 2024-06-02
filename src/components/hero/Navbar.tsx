aa65d11c785400429bf4ea2aa4bf2c94458b7835
JMenke <jacqueline.menke@outlook.de>
2024-06-02 18:09:14 +0200

    Navbar fix for mobile screen

--- src/components/hero/Navbar.tsx
+++ src/components/hero/Navbar.tsx
@@ -4,35 +4,20 @@ const Navbar: FC = () => {
   return (
     <nav className='navbar'>
       <div className='nav-links'>
-        <a
-          href='https://jacqueline-menke.netlify.app/#home'
-          className='nav-link'
-        >
+        <a href='#home' className='nav-link'>
           <span>home</span>
         </a>
-        <a
-          href='https://jacqueline-menke.netlify.app/#about'
-          className='nav-link'
-        >
+        <a href='#about' className='nav-link'>
           <span>about</span>
         </a>
-        <a
-          href='https://jacqueline-menke.netlify.app/#experience'
-          className='nav-link'
-        >
+        <a href='#experience' className='nav-link'>
           <span>experience</span>
         </a>
-        <a
-          href='https://jacqueline-menke.netlify.app/#skills'
-          className='nav-link'
-        >
+        <a href='#skills' className='nav-link'>
           <span>skills</span>
         </a>
-        <a
-          href='https://jacqueline-menke.netlify.app/#projects'
-          className='nav-link'
-        >
-          <span>projects</span>
+        <a href='#projects' className='nav-link'>
+          <span>work projects</span>
         </a>
       </div>
     </nav>
