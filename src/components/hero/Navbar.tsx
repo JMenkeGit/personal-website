acda73e00e77c3e95b0d27bc036fbcb85ebf1feb
JMenke <jacqueline.menke@outlook.de>
2024-06-02 13:43:30 +0200

    Fixed link anchors for mobile view

--- src/components/hero/Navbar.tsx
+++ src/components/hero/Navbar.tsx
@@ -4,19 +4,34 @@ const Navbar: FC = () => {
   return (
     <nav className='navbar'>
       <div className='nav-links'>
-        <a href='#home' className='nav-link'>
+        <a
+          href='https://jacqueline-menke.netlify.app/#home'
+          className='nav-link'
+        >
           <span>home</span>
         </a>
-        <a href='#about' className='nav-link'>
+        <a
+          href='https://jacqueline-menke.netlify.app/#about'
+          className='nav-link'
+        >
           <span>about</span>
         </a>
-        <a href='#experience' className='nav-link'>
+        <a
+          href='https://jacqueline-menke.netlify.app/#experience'
+          className='nav-link'
+        >
           <span>experience</span>
         </a>
-        <a href='#skills' className='nav-link'>
+        <a
+          href='https://jacqueline-menke.netlify.app/#skills'
+          className='nav-link'
+        >
           <span>skills</span>
         </a>
-        <a href='#projects' className='nav-link'>
+        <a
+          href='https://jacqueline-menke.netlify.app/#projects'
+          className='nav-link'
+        >
           <span>work projects</span>
         </a>
       </div>
